158587a50c8bc384bbe58b31911a499db38c087c
Alex Tochetto <alextochetto@gmail.com>
2023-01-13 12:55:47 -0300

    simulation repository

---
+++ SimulationConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimulationConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void CheckAppointmentInverted()
+        {
+            List<int> avaliableAppointments = new List<int>{ 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+            List<int> scheduledAppointments = new List<int>{ 9, 10, 11, 18};
+        }
+    }
+}
