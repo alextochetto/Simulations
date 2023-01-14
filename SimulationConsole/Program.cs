cb47e4c084cc3a672d3f2f940e5dbbe065ce6160
Alex Tochetto <alextochetto@gmail.com>
2023-01-14 10:52:59 -0300

    inverted logic

--- SimulationConsole/Program.cs
+++ SimulationConsole/Program.cs
@@ -8,12 +8,27 @@ namespace SimulationConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            CheckAppointmentInverted();
         }
 
         static void CheckAppointmentInverted()
         {
-            List<int> avaliableAppointments = new List<int>{ 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+            List<int> avaliables = new List<int>{ 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
             List<int> scheduledAppointments = new List<int>{ 9, 10, 11, 18};
+
+            List<int> avaliableAppointments = new List<int>();
+            foreach (int hour in avaliables)
+            {
+                int hourFind = scheduledAppointments.Find(x => x == hour);
+                if (hourFind == 0)
+                {
+                    avaliableAppointments.Add(hour);
+                }
+            }
+            foreach (int item in avaliableAppointments)
+            {
+                Console.WriteLine("Hour avaliable: " + item);
+            }
         }
     }
 }
